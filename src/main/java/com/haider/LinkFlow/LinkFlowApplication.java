bd47df1c51d9a17fa6e489f2c104fb6d658b5aab
sh8941 <zshamoonhaider@gmail.com>
2026-06-05 18:14:14 +0530

    inital project

---
+++ src/main/java/com/haider/LinkFlow/LinkFlowApplication.java
@@ -0,0 +1,13 @@
+package com.haider.LinkFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haider/LinkFlow/LinkFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haider.LinkFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
